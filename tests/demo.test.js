98631a16e0e4be39805ea6b4788a536beb7a56a4
Robinson Betancur Marin <robinsonb@iadb.org>
2022-11-07 13:16:34 -0500

    test in 02-template-string

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,12 @@
-test("Esta prueba no debe de fallar", () => {
-  if (0 === 1) {
-    throw new Error("No puede dividir entre cero");
-  }
+describe("Pruebas en <DemoComponent />", () => {
+  test("Esta prueba no debe de fallar", () => {
+    // 1. Inicializacion
+    const message1 = "hola Mundo";
+
+    // 2. Estimulo
+    const message2 = message1.trim();
+
+    // 3. Observar el comportamiento esperado
+    expect(message1).toBe(message2);
+  });
 });
